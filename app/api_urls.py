244a046924f3702b45637dc135f4eb3f684db579
Pravin <pravin@example.com>
2026-02-09 15:39:47 +0530

    School Create API added

--- app/api_urls.py
+++ app/api_urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import SchoolListView
-
+from .views import SchoolListView, ShoolCreateView
+    
 urlpatterns = [
     path('schools/', SchoolListView.as_view(), name='school-list'),
+    path('schools/create/', ShoolCreateView.as_view(), name='school-create'),
 ]       
--- app/views.py
+++ app/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 from .models import School
 from .serializers import SchoolSerializer
 from rest_framework.generics import ListAPIView
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 def index(request):
     return HttpResponse("Hello, world pravin!\n")
@@ -11,3 +13,13 @@ def index(request):
 class SchoolListView(ListAPIView):
     queryset = School.objects.all()
     serializer_class = SchoolSerializer
+
+
+class ShoolCreateView(APIView):
+    def post(self, request):
+        serializer = SchoolSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
+
